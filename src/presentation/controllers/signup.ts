8515e739190162a72f009eb6fde89f07dd2b53e4
Andrew Rosário <andrew.arosario@gmail.com>
2020-04-25 13:41:43 -0300

    fix: add body in http-request

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,13 +1,13 @@
 export class SignUpController {
   handle (httpRequest: any): any {
-    if (!httpRequest.name) {
+    if (!httpRequest.body.name) {
       return {
         statusCode: 400,
         body: new Error('Missing param: name')
       }
     }
 
-    if (!httpRequest.email) {
+    if (!httpRequest.body.email) {
       return {
         statusCode: 400,
         body: new Error('Missing param: email')
