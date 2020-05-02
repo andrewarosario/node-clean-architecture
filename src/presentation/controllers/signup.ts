47d04cccff13880f4a96d6b26657171dfa20929f
Andrew Rosário <andrew.arosario@gmail.com>
2020-05-02 10:18:01 -0300

    refactor: remove code duplicated

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -18,11 +18,13 @@ export class SignUpController implements Controller {
         }
       }
 
-      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+      const { email, password, passwordConfirmation } = httpRequest.body
+
+      if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'))
       }
 
-      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      const isValid = this.emailValidator.isValid(email)
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
