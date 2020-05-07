1fcdbaf6200d62b03aa7f56c6fae0a678109f3ec
Andrew Rosário <andrew.arosario@gmail.com>
2020-05-07 06:42:51 -0300

    refactor: move success http response to a helper method

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,6 +1,11 @@
 import { HttpResponse } from '../protocols/http'
 import { ServerError } from '../errors/server-error'
 
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
+
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
