e367ee6496fd8b74e942d1ea30b28a767bff086c
Andrew Rosário <andrew.arosario@gmail.com>
2020-06-10 07:05:47 -0300

    test: ensure mongodb reconnect if client is down

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -11,7 +11,7 @@ afterAll(async () => {
 })
 
 beforeEach(async () => {
-  const accountCollection = MongoHelper.getCollection('accounts')
+  const accountCollection = await MongoHelper.getCollection('accounts')
   await accountCollection.deleteMany({})
 })
 
