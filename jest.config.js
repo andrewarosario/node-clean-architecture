139cb16e7ed13a70e08d25c191835f1c21f5a383
Andrew Rosário <andrew.arosario@gmail.com>
2020-06-30 13:01:03 -0300

    chore: ignoring protocols on coverage tests

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,9 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/main/**'
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**'
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
