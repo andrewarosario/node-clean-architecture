e6ef9f4502a219e6db0d9638243a84150071fb35
Andrew Rosário <andrew.arosario@gmail.com>
2020-07-08 08:35:11 -0300

    test: ensure ValidationComposite not return if validation succeeds

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -37,4 +37,10 @@ describe('Validation Composite', () => {
     const error = sut.validate({ field: 'any_value' })
     expect(error).toEqual(new Error())
   })
+
+  test('Should not return validation succeeds', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toBeFalsy()
+  })
 })
