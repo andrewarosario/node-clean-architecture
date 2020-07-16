640b5f2e9918ce1b9a916a26954914b72d95d104
Andrew Rosário <andrew.arosario@gmail.com>
2020-07-16 06:58:07 -0300

    test: add login route

--- src/main/routes/auth-routes.test.ts
+++ src/main/routes/auth-routes.test.ts
@@ -1,6 +1,10 @@
 import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
+
+let accountCollection: Collection
 
 describe('Auth Routes', () => {
   beforeAll(async () => {
@@ -12,7 +16,7 @@ describe('Auth Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -25,6 +29,25 @@ describe('Auth Routes', () => {
           email: 'andrew.arosario@gmail.com',
           password: '123',
           passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return 200 on login', async () => {
+      const password = await hash('123', 12)
+      await accountCollection.insertOne({
+        name: 'Andrew',
+        email: 'andrew.arosario@gmail.com',
+        password
+      })
+
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'andrew.arosario@gmail.com',
+          password: '123'
         })
         .expect(200)
     })
