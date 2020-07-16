4fe81c7f97b38ab340314e9ff646ce5d3a8768dc
Andrew Rosário <andrew.arosario@gmail.com>
2020-07-16 06:35:03 -0300

    feat: add login factory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.SECRET || 'u7G6A0=8cpQ6'
 }
