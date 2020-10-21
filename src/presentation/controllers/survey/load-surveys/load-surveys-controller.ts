3eb061df81af27ae8f2ff41c64b7ac097f0b1f3c
Andrew Rosário <andrew.arosario@gmail.com>
2020-10-21 20:05:25 -0300

    feat: ensure LoadSurveysController returns 204 if LoadSurveys returns empty

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -1,4 +1,4 @@
-import { ok, serverError } from '../../../helpers/http/http-helper'
+import { noContent, ok, serverError } from '../../../helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, LoadSurveysControllerModel, LoadSurveys } from './load-surveys-controller-protocols'
 
 export class LoadSurveysController implements Controller<LoadSurveysControllerModel> {
@@ -9,7 +9,7 @@ export class LoadSurveysController implements Controller<LoadSurveysControllerMo
   async handle (httpRequest: HttpRequest<LoadSurveysControllerModel>): Promise<HttpResponse> {
     try {
       const surveys = await this.loadSurveys.load()
-      return ok(surveys)
+      return surveys.length ? ok(surveys) : noContent()
     } catch (error) {
       return serverError(error)
     }
