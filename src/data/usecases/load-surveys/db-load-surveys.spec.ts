0e5f19732d1ca87f18772b5ad0712fc459006cb6
Andrew Rosário <andrew.arosario@gmail.com>
2020-10-23 07:50:49 -0300

    test: ensure DbLoadSurveys return a list of surveys on success

--- src/data/usecases/load-surveys/db-load-surveys.spec.ts
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -44,4 +44,11 @@ describe('DbLoadSurveys UseCase', () => {
     await sut.load()
     expect(loadAllSpy).toHaveBeenCalled()
   })
+
+  test('Should return a list of surveys on success', async () => {
+    const { sut } = makeSut()
+
+    const surveys = await sut.load()
+    expect(surveys).toEqual(makeFakeSurveys())
+  })
 })
