7c1b6923e4c76d7f48b1522e0ec754883e0433c1
Andrew Rosário <andrew.arosario@gmail.com>
2020-10-29 06:46:23 -0300

    feat: ensure SurveyMongoRepository load all surveys

--- src/data/usecases/load-surveys/db-load-surveys.ts
+++ src/data/usecases/load-surveys/db-load-surveys.ts
@@ -1,6 +1,6 @@
 import { SurveyModel } from '../../../domain/models/survey'
 import { LoadSurveys } from '../../../domain/usecase/load-surveys'
-import { LoadSurveysRepository } from '../../protocols/db/survey/load-surveys-repository copy'
+import { LoadSurveysRepository } from '../../protocols/db/survey/load-surveys-repository'
 
 export class DbLoadSurveys implements LoadSurveys {
   constructor (
