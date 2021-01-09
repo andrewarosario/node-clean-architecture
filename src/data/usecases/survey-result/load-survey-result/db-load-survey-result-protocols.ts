3ec3ae6fef6d2778d699102eeae0ab18bbf8358c
Andrew Rosário <andrew.arosario@gmail.com>
2021-01-09 16:04:38 -0300

    feat: ensure DbLoadSurveyResult calls LoadSurveyByIdRepo if LoadSurveyResultRepo returns null

--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result-protocols.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result-protocols.ts
@@ -1,3 +1,4 @@
 export * from '@/data/protocols/db/survey-result/load-survey-result-repository'
+export * from '@/data/protocols/db/survey/load-survey-by-id-repository'
 export * from '@/domain/models/survey-result'
 export * from '@/domain/usecase/survey-result/load-survey-result'
