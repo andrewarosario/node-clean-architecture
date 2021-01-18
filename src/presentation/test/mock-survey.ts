c91c573ffde3d65baf24dfae0c81d6f55ab12f19
Andrew Rosário <andrew.arosario@gmail.com>
2021-01-18 07:27:03 -0300

    test: change load surveys to add didAnswer field

--- src/presentation/test/mock-survey.ts
+++ src/presentation/test/mock-survey.ts
@@ -15,10 +15,10 @@ export class AddSurveySpy implements AddSurvey {
 
 export class LoadSurveysSpy implements LoadSurveys {
   surveyModels = mockSurveyModels()
-  callsCount = 0
+  accountId: string
 
-  async load (): Promise<SurveyModel[]> {
-    this.callsCount++
+  async load (accountId: string): Promise<SurveyModel[]> {
+    this.accountId = accountId
     return Promise.resolve(this.surveyModels)
   }
 }
