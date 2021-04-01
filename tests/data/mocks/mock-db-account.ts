d7e7429d096f03b39aea552b85caa696451df333
Andrew Rosário <andrew.arosario@gmail.com>
2021-04-01 10:42:37 -0300

    refactor: add CheckAccountByEmailRepository

--- tests/data/mocks/mock-db-account.ts
+++ tests/data/mocks/mock-db-account.ts
@@ -1,4 +1,4 @@
-import { AddAccountRepository, LoadAccountByEmailRepository, LoadAccountByTokenRepository, UpdateAccessTokenRepository } from '@/data/protocols'
+import { AddAccountRepository, CheckAccountByEmailRepository, LoadAccountByEmailRepository, LoadAccountByTokenRepository, UpdateAccessTokenRepository } from '@/data/protocols'
 
 import faker from 'faker'
 
@@ -22,6 +22,16 @@ export class LoadAccountByEmailRepositorySpy implements LoadAccountByEmailReposi
   email: string
 
   async loadByEmail (email: string): Promise<LoadAccountByEmailRepository.Result> {
+    this.email = email
+    return this.result
+  }
+}
+
+export class CheckAccountByEmailRepositorySpy implements CheckAccountByEmailRepository {
+  result = false
+  email: string
+
+  async checkByEmail (email: string): Promise<CheckAccountByEmailRepository.Result> {
     this.email = email
     return this.result
   }
