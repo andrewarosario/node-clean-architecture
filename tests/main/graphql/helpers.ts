cff58f3ce7c2af2ab50b1ba14e9826d86683c1db
Andrew Rosário <andrew.arosario@gmail.com>
2021-04-24 17:17:44 -0300

    test: ensure Login Query returns an account on valid credentials

---
+++ tests/main/graphql/helpers.ts
@@ -0,0 +1,10 @@
+import { ApolloServer } from 'apollo-server-express'
+import resolvers from '@/main/graphql/resolvers'
+import typeDefs from '@/main/graphql/type-defs'
+import schemaDirectives from '@/main/graphql/directives'
+
+export const makeApolloServer = (): ApolloServer => new ApolloServer({
+  resolvers,
+  typeDefs,
+  schemaDirectives
+})
