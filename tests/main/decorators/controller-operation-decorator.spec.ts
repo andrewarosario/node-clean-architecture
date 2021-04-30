df0e33ee2ac9c6a0884b956bf4b309406e37c8fc
Andrew Rosário <andrew.arosario@gmail.com>
2021-04-30 19:02:00 -0300

    feat: ensure ControllerOperationDecorator returns 500 if controller throws

--- tests/main/decorators/controller-operation-decorator.spec.ts
+++ tests/main/decorators/controller-operation-decorator.spec.ts
@@ -1,4 +1,6 @@
 import { ControllerOperationDecorator } from '@/main/decorators'
+import { throwError } from '@/tests/domain/mocks'
+import { mockServerError } from '@/tests/presentation/mocks'
 import { ControllerSpy } from '@/tests/presentation/mocks/controller'
 import faker from 'faker'
 
@@ -22,5 +24,19 @@ describe('ControllerOperation Decorator', () => {
     const request = faker.lorem.sentence()
     await sut.handle(request)
     expect(controllerSpy.request).toEqual(request)
+  })
+
+  test('Should return the same result of the controller on success', async () => {
+    const { sut, controllerSpy } = makeSut()
+    const httpResponse = await sut.handle(faker.lorem.sentence())
+    expect(httpResponse).toEqual(controllerSpy.httpResponse)
+  })
+
+  test('Should return 500 if controller throws', async () => {
+    const { sut, controllerSpy } = makeSut()
+    jest.spyOn(controllerSpy, 'handle').mockImplementationOnce(throwError)
+    const serverError = mockServerError()
+    const httpResponse = await sut.handle(faker.lorem.sentence())
+    expect(httpResponse).toEqual(serverError)
   })
 })
