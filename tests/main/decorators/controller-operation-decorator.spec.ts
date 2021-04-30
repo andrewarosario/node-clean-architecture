e2093f5268c85ac4caa31915741d4b932d43c939
Andrew Rosário <andrew.arosario@gmail.com>
2021-04-30 18:39:18 -0300

    refactor: move ControllerOperationDecorator creation to a factory method

--- tests/main/decorators/controller-operation-decorator.spec.ts
+++ tests/main/decorators/controller-operation-decorator.spec.ts
@@ -2,11 +2,23 @@ import { ControllerOperationDecorator } from '@/main/decorators'
 import { ControllerSpy } from '@/tests/presentation/mocks/controller'
 import faker from 'faker'
 
+type SutTypes = {
+  sut: ControllerOperationDecorator
+  controllerSpy: ControllerSpy
+}
+
+const makeSut = (): SutTypes => {
+  const controllerSpy = new ControllerSpy()
+  const sut = new ControllerOperationDecorator(controllerSpy)
+  return {
+    sut,
+    controllerSpy
+  }
+}
+
 describe('ControllerOperation Decorator', () => {
   test('Should call controller handle', async () => {
-    const controllerSpy = new ControllerSpy()
-    const sut = new ControllerOperationDecorator(controllerSpy)
-
+    const { sut, controllerSpy } = makeSut()
     const request = faker.lorem.sentence()
     await sut.handle(request)
     expect(controllerSpy.request).toEqual(request)
